2ff0d5e961a2511ead88a53c943eecdde945aebe
Mikko Leppänen <mleppan23@gmail.com>
2014-08-17 18:24:50 +0300

    fucntion comment added

--- qml/pages/scripts/componentCreation.js
+++ qml/pages/scripts/componentCreation.js
@@ -17,6 +17,8 @@
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// Create Area.qml object dynamically
+
 function createAreaObject(cw,ch) {
     var component = Qt.createComponent(Qt.resolvedUrl("../content/Area.qml"));
     if (component.status === Component.Ready) {
